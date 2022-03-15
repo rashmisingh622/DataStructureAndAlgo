45b6ebc5df8fd6353adde9c93c8c8f06b0014617
Rashmi Singh <rassingh@microsoft.com>
2022-03-15 15:05:43 +0530

    changes

---
+++ Backtracking/BacktrackingHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backtracking
+{
+    public class BacktrackingHelper
+    {
+        public void PrintNum(int n)
+        {
+            if (n == 6)
+            {
+                return;
+            }
+
+            Console.WriteLine(n);
+
+            PrintNum(n + 1);
+        }
+
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+
+            return list;
+        }
+
+        public IList<IList<int>> Permute(int[] nums, int i, IList<IList<int>> result)
+        {
+            if (i == nums.Length - 1)
+            {
+                result.Add(new List<int> { nums[i] });
+            }
+        }
+    }
+}
---
+++ Backtracking/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Backtracking;
+
+Console.WriteLine("Hello, World!");
+var p = new BacktrackingHelper();
+p.PrintNum(0);
+Console.ReadLine();
